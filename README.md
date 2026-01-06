28d951e81281248ac5b79a1ce4911d827f74b86b
johntalton <johntalton@gmail.com>
2026-01-06 05:19:39 -0500

    🍳

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# Settee
+# 🛋️ Settee DB<sup>*</sup>
+
+An attempt to baseline the most naïve implementation of a NoSQL JSON document store inspired by other furniture based solutions.
+
+<sub>* and by DB, we mean not-at-all a DB</sub>
+
+## Concept
+
+Settee store documents as raw JSON under individually named files by `id`. with the addition of `"settee:id"` and `"settee:revision"` properties.
+
+All function operate directly on the File System (FS) via standard OS calls, allowing for other tools to operate on the data (tools like `grep` or `jq` etc).
+
+Crafting `id` values is left up to the caller and no internal concept exists.
+
+Versioning is handled via the `revision` tag which is passed to any function which need to modify the data (a `base64` encoded `SHA-1` of the data).
+
+## Overview
+
+This package exports a singular static class `Settee`.
+
+A set of standard `Promise` based functions can be found: `get`, `set`, `update`, `delete`, `has`, `isModified`.
+
+Each function takes a mix of `id`, `revision`, `document` and an `options`
+
+The `options` object allow customization of the directory/file storage location (per function), as well as `timeoutMs` and `signal` (The timeout signal will always be applied regardless of user supplied `signal`, though not all function support signaling).
+
+If `options` are not specified, `process.env` values are used as a fallback.
+
+
+## Example
+
+
+```js
+import { Settee } from '@johntalton/settee'
+
+// override default and process.env values
+const options = {
+  db: 'data/example',
+  timeoutMs: 10,
+}
+
+// some id we choose
+const id = crypto.randomUUID()
+
+//
+const has = await Settee.has(id, options) // false
+
+// create a new document
+const doc = await Settee.set(id, {
+  type: 'example.v1',
+  name: 'fav-colors',
+  color: [ 'red', 'yellow', 'orange' ]
+}, options)
+
+// cache of the assigned revision for later
+const revision = doc['settee:revision']
+
+// check if the file has been modified (aka revision match)
+const modified = await Settee.isModified(
+  id,
+  revision,
+  options)
+
+// cleanup
+await Settee.delete(id, revision, options)
+```
+
+## Map
+
+Mapping currently exists as a full scan via the `map` function.
+
+The callback map function should return `Array<[ key, value ]>` or `undefined`.
+
+The map function can take in a `string|Array<string>|undefined` as a key Filter to be performed during the map.
+
+```js
+const result = await Settee.map(function (doc)  {
+  if(doc.type !== 'example.v1') { return }
+
+  const colors = doc.colors ?? []
+  return colors.map(color => [ color, 1 ])
+})
+/*
+{
+  rows: [
+    { key: 'yellow', value: 1, settee:id: ... },
+    { key: 'green', value: 1, settee:id: ... },
+    // ...
+  ]
+}
+*/
+```
--- src/settee.js
+++ src/settee.js
@@ -7,8 +7,15 @@ import * as crypto from 'node:crypto'
 
 /**
  * @typedef {Object} SetteeOptionsResolved
- * @property {string} documentRoot
+ * @property {string} db
+ * @property {string} documentsDir
+ * @property {string} documentsRoot
+ * @property {string} viewsDir
+ * @property {string} viewsRoot
+ * @property {number} timeoutMs
  * @property {AbortSignal} signal
+ *
+ * @property {AbortSignal|undefined} [internal_signal] Overrides signal usage and creation
  */
 
 /** @typedef {Partial<SetteeOptionsResolved>} SetteeOptions */
@@ -19,32 +26,65 @@ import * as crypto from 'node:crypto'
  */
 
 
-export const SETTEE_ENV_DOCUMENT_ROOT= 'SETTEE_ROOT'
-export const SETTEE_ENV_TIMEOUT = 'SETTEE_TIMEOUT'
+export const SETTEE_ENV = {
+	DB_ROOT: 'SETTEE_DB',
+	DOCUMENTS_DIRECTORY: 'SETTEE_DOCS_DIR',
+	VIEWS_DIRECTORY: 'SETTEE_VIEWS_DIR',
 
-export const SETTEE_DEFAULT_TIMEOUT_MS = '100'
-export const SETTEE_DEFAULT_FILE_EXTENSION = 'json'
+	TIMEOUT_MS: 'SETTEE_TIMEOUT'
+}
 
-export const SETTEE_DEFAULT_ROOT = './documents'
+
+export const SETTEE_DEFAULT = {
+	ROOT: './db',
+	DOCS: './docs',
+	VIEWS: './views',
+
+	FILE_EXTENSION: 'json',
+	DIGEST_ALGO: 'SHA-1',
+	TIMEOUT_MS: '100'
+}
 
 export const SETTEE_INTERNAL_PROPERTIES = {
 	ID: 'settee:id',
 	REVISION: 'settee:revision'
 }
 
-const ENV = {
-	ROOT: process.env[SETTEE_ENV_DOCUMENT_ROOT] ?? SETTEE_DEFAULT_ROOT,
-	TIMEOUT: parseInt(process.env[SETTEE_ENV_TIMEOUT] ?? SETTEE_DEFAULT_TIMEOUT_MS, 10)
-}
+const ENV_ROOT = process.env[SETTEE_ENV.DB_ROOT] ?? SETTEE_DEFAULT.ROOT
+const ENV_DOCS_DIR = process.env[SETTEE_ENV.DOCUMENTS_DIRECTORY] ?? SETTEE_DEFAULT.DOCS
+const ENV_VIEWS_DIR = process.env[SETTEE_ENV.VIEWS_DIRECTORY] ?? SETTEE_DEFAULT.VIEWS
+const ENV_TIMEOUT_MS = parseInt(process.env[SETTEE_ENV.TIMEOUT_MS] ?? SETTEE_DEFAULT.TIMEOUT_MS, 10)
 
 /**
  * @param {SetteeOptions|undefined} [options]
  * @returns {SetteeOptionsResolved}
  */
 export function fromOptions(options){
+	const db = options?.db ?? ENV_ROOT
+	const documentsDir = options?.documentsDir ?? ENV_DOCS_DIR
+	const documentsRoot = options?.documentsRoot ?? path.join(db, documentsDir)
+	const viewsDir = options?.viewsDir ?? ENV_VIEWS_DIR
+	const viewsRoot = options?.viewsRoot ?? path.join(db, viewsDir)
+	const timeoutMs = options?.timeoutMs ?? ENV_TIMEOUT_MS
+
+	if(Number.isFinite(timeoutMs)) {}
+	if(timeoutMs <= 0) {}
+
+	// if we have an internal signal, use that, otherwise
+	//  combined optional signal with timeout or just timeout
+	const signal = options?.internal_signal ?? ((options?.signal === undefined) ?
+		AbortSignal.timeout(timeoutMs) :
+		AbortSignal.any([ options.signal, AbortSignal.timeout(timeoutMs) ]))
+
 	return {
-		documentRoot: options?.documentRoot ?? ENV.ROOT,
-		signal: options?.signal ?? AbortSignal.timeout(ENV.TIMEOUT)
+		db,
+		documentsDir,
+		viewsDir,
+		documentsRoot,
+		viewsRoot,
+		timeoutMs,
+		signal,
+		internal_signal: options?.internal_signal
 	}
 }
 
@@ -53,6 +93,8 @@ export function fromOptions(options){
  * @returns {id is DocId}
  */
 export function isValidDocId(id) {
+	if(id === undefined) { return false }
+	if(id === '') { return false }
 	return true
 }
 
@@ -61,22 +103,45 @@ export function isValidDocId(id) {
  * @returns {id is RevisionId}
  */
 export function isValidRevisionId(id) {
+	if(id === undefined) { return false }
+	if(id === '') { return false }
 	return true
 }
 
 /**
- * @param {string} documentRoot
+ * @param {string} documentsRoot
  * @param {DocId} id
  */
-export function pathFromId(documentRoot, id) {
+export function pathFromId(documentsRoot, id) {
 	if(id.includes(path.sep)) { throw new Error('invalid id (sep)') }
 	if(id.includes(path.delimiter)) { throw new Error('invalid id (delimiter)') }
 
 	return path.normalize(path.format({
-		dir: documentRoot,
+		dir: documentsRoot,
 		name: id,
-		ext: SETTEE_DEFAULT_FILE_EXTENSION
+		ext: SETTEE_DEFAULT.FILE_EXTENSION
 	}))
+}
+
+/**
+ * @template T
+ * @param {Document<T>|T} doc
+ * @returns {Promise<RevisionId>}
+ */
+export async function revisionFrom(doc) {
+	const json = JSON.stringify({
+		...doc,
+		[SETTEE_INTERNAL_PROPERTIES.ID]: undefined,
+		[SETTEE_INTERNAL_PROPERTIES.REVISION]: undefined,
+	})
+	const encoder = new TextEncoder()
+	const buffer = encoder.encode(json)
+
+	const hash = await crypto.subtle.digest(SETTEE_DEFAULT.DIGEST_ALGO, buffer)
+	const hash8 = new Uint8Array(hash)
+
+	/** @ts-ignore */
+	return hash8.toBase64()
 }
 
 /**
@@ -86,21 +151,25 @@ export function pathFromId(documentRoot, id) {
  * @returns {Promise<Document<T>>}
  */
 export async function internalDocFrom(id, doc) {
-	const json = JSON.stringify(doc)
-	const encoder = new TextEncoder()
-	const buffer = encoder.encode(json)
-
-	const hash = await crypto.subtle.digest('SHA-1', buffer)
-	const hash8 = new Uint8Array(hash)
-	const hashHex = hash8.toBase64()
-
+	const revision = await revisionFrom(doc)
 	const internalDoc = {
 		...doc,
 		[SETTEE_INTERNAL_PROPERTIES.ID]: id,
-		[SETTEE_INTERNAL_PROPERTIES.REVISION]: hashHex,
+		[SETTEE_INTERNAL_PROPERTIES.REVISION]: revision,
 	}
 
 	return internalDoc
+}
+
+/**
+ * @template T
+ * @param {Document<T>} doc
+ * @returns {Promise<boolean>}
+ */
+export async function internalDocHasIntegrity(doc) {
+	const revision = doc[SETTEE_INTERNAL_PROPERTIES.REVISION]
+	const computedRevision = await revisionFrom(doc)
+	return computedRevision === revision
 }
 
 export class Settee {
@@ -108,7 +177,23 @@ export class Settee {
 	 * @param {SetteeOptions|undefined} [options]
 	 */
 	static async* changes(options) {
+		const {
+			documentsRoot,
+			signal
+		} = fromOptions(options)
 
+		try {
+			const watcher = fs.watch(documentsRoot, { signal })
+
+			for await (const { eventType, filename } of watcher) {
+				//
+				console.log('watch:', eventType, filename)
+			}
+		}
+		catch(e) {
+			// if(e.name === 'AbortError') { return }
+			throw e
+		}
 	}
 
 	/**
@@ -118,10 +203,10 @@ export class Settee {
 	 */
 	static async has(id, options) {
 		const {
-			documentRoot
+			documentsRoot
 		} = fromOptions(options)
 
-		const filepath = pathFromId(documentRoot, id)
+		const filepath = pathFromId(documentsRoot, id)
 
 		return fs.access(filepath, fs.constants.F_OK)
 			.then(() => true)
@@ -136,6 +221,7 @@ export class Settee {
 	 */
 	static async isModified(id, revision, options) {
 		const internalDoc = await Settee.get(id, options)
+		if(!internalDocHasIntegrity(internalDoc)) { return true }
 		return internalDoc[SETTEE_INTERNAL_PROPERTIES.REVISION] !== revision
 	}
 
@@ -147,13 +233,13 @@ export class Settee {
 	 */
 	static async get(id, options) {
 		const {
-			documentRoot,
+			documentsRoot,
 			signal
 		} = fromOptions(options)
 
-		const filepath = pathFromId(documentRoot, id)
+		const filepath = pathFromId(documentsRoot, id)
 
-		const json = await fs.readFile(filepath, { encoding: 'utf8', flag: 'r', signal})
+		const json = await fs.readFile(filepath, { encoding: 'utf8', flag: 'r', signal })
 		return JSON.parse(json)
 	}
 
@@ -166,11 +252,11 @@ export class Settee {
 	 */
 	static async set(id, doc, options) {
 		const {
-			documentRoot,
+			documentsRoot,
 			signal
 		} = fromOptions(options)
 
-		const filepath = pathFromId(documentRoot, id)
+		const filepath = pathFromId(documentsRoot, id)
 		const internalDoc = await internalDocFrom(id, doc)
 		const internalJson = JSON.stringify(internalDoc)
 
@@ -188,23 +274,18 @@ export class Settee {
 		const id =  doc[SETTEE_INTERNAL_PROPERTIES.ID]
 		const revision = doc[SETTEE_INTERNAL_PROPERTIES.REVISION]
 
-		if(id === undefined) { throw new Error('missing id') }
 		if(!isValidDocId(id)) { throw new Error('invalid doc id') }
-
-		if(revision === undefined) { throw new Error('missing revision') }
 		if(!isValidRevisionId(revision)) { throw new Error('invalid revision id') }
 
-		const modified = await this.isModified(id, revision)
-		if(modified) {
-			throw new Error('revision miss-match')
-		}
-
 		const {
-			documentRoot,
+			documentsRoot,
 			signal
 		} = fromOptions(options)
 
-		const filepath = pathFromId(documentRoot, id)
+		const modified = await Settee.isModified(id, revision, { ...options, internal_signal: signal })
+		if(modified) { throw new Error('revision miss-match') }
+
+		const filepath = pathFromId(documentsRoot, id)
 		const internalDoc = await internalDocFrom(id, doc)
 		const internalJson = JSON.stringify(internalDoc)
 
@@ -218,16 +299,71 @@ export class Settee {
 	 * @param {SetteeOptions|undefined} [options]
 	 */
 	static async delete(id, revision, options) {
-		const modified = await this.isModified(id, revision)
-		if(modified) {
-			throw new Error('revision miss-match')
-		}
-
 		const {
-			documentRoot
+			documentsRoot,
+			signal
 		} = fromOptions(options)
 
-		const filepath = pathFromId(documentRoot, id)
+		const modified = await Settee.isModified(id, revision, { ...options, internal_signal: signal })
+		if(modified) { throw new Error('revision miss-match') }
+
+		const filepath = pathFromId(documentsRoot, id)
+
 		return fs.rm(filepath, { maxRetries: 0 })
+	}
+
+	/**
+	 * @template T
+	 * @param {(doc: Document<T>) => [ key: any, value: any]} fn
+	 * @param {string|Array<string>|undefined} [filterKey]
+	 * @param {SetteeOptions|undefined} [options]
+	 */
+	static async map(fn, filterKey, options) {
+		const {
+			documentsRoot,
+			signal
+		} = fromOptions(options)
+
+		const filterKeyList = filterKey === undefined ? [] : Array.isArray(filterKey) ? filterKey : [ filterKey ]
+
+		const result = new Map()
+
+		const listing = await fs.readdir(documentsRoot, { withFileTypes: true, encoding: 'utf8' })
+		for (const entry of listing) {
+			if(signal.aborted) { break }
+			if(!entry.isFile()) { continue }
+
+			// console.log('mapping over', entry.name)
+			const filepath = path.join(documentsRoot, entry.name)
+			const json = await fs.readFile(filepath, { encoding: 'utf8', flag: 'r', signal })
+			const internalDoc = JSON.parse(json)
+
+			const id = internalDoc[SETTEE_INTERNAL_PROPERTIES.ID]
+
+			const keySet = await Promise.try(fn, internalDoc)
+			if(filterKeyList.length === 0) {
+				result.set(id, keySet)
+			}
+			else {
+				const filteredKeySet = keySet?.filter(item => filterKeyList.includes(item[0]))
+				result.set(id, filteredKeySet)
+			}
+		}
+
+		if(signal.aborted) { return }
+
+		const output = result
+			.entries()
+			.filter(entry => entry[1] !== undefined)
+			.map(entry => entry[1].map(item => ({ [SETTEE_INTERNAL_PROPERTIES.ID]: entry[0], key: item[0], value: item[1] })))
+			.reduce((accumulator, value) => {
+				value.forEach(element => {
+					accumulator.push(element)
+				})
+
+				return accumulator
+			}, [])
+
+		return { rows: output }
 	}
 }
